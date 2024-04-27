6c369e8c0614138be414405dd6a6f9d15afa35c5
DeeBest <simphiwedladla8@gmail.com>
2024-04-27 08:55:07 +0200

    added a function to update total characters counter

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const footerParagraph = document.getElementById('footer-paragraph');
 const currentDate = new Date().getFullYear();
 footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+
+const textarea = document.getElementById('textarea');
+const totalCounter = document.getElementById('total-characters-counter');
+
+textarea.addEventListener('keyup', () => {
+  updateTotalCounter();
+});
+
+function updateTotalCounter() {
+  totalCounter.textContent = textarea.value.length;
+}
